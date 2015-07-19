8880bff1dab66bbd973278d0919665802c9a1164
AhmedMH <enghamed126@gmail.com>
2015-07-19 17:03:51 +0200

    Second commit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=news_portal',
+    'dsn' => 'mysql:host=localhost;dbname=newsportal',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
