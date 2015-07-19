0e23e04235eb81a733736e61add715b6098649c8
AhmedMH <enghamed126@gmail.com>
2015-07-19 17:27:28 +0200

    Password

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -7,7 +7,7 @@ use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\User;
-use app\models\password;
+use app\models\Password;
 use app\models\Mailer;
 use app\models\SignupForm;
 use app\models\LoginForm;
